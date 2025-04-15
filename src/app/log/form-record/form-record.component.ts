8d63ed69cc4579e14f9b9c3de5469fa1c98522d0
TigerTigre88 <luposolitario1712@gmail.com>
2025-04-15 09:19:26 +0200

    feat(navbar):insert nav in offert

--- src/app/log/form-record/form-record.component.ts
+++ src/app/log/form-record/form-record.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {BookserviceService} from '../../services/bookservice.service';
 
 @Component({
   selector: 'app-form-record',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './form-record.component.html',
   styleUrl: './form-record.component.css'
 })
 export class FormRecordComponent {
+  userForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private userService : BookserviceService) {
+    this.userForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+  onSubmit() {
+    if (this.userForm.valid) {
+      this.userService.signup(this.userForm.value).subscribe(
+        response => console.log('User signed up!', response),
+        error => console.error('Signup failed:', error)
+      );
+    }
+  }
 }
