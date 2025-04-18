5f59ba630d6d25333bffb397017037659cf5d9d5
Sarrus95 <rosariozago@outlook.it>
2025-04-18 15:35:00 +0200

    fix(app.routes): :bug: Fixed routes
    Fixed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookDetailsComponent } from './book-details/book-details.component';
-import {AuthComponent} from './log/auth/auth.component';
-import {FormRecordComponent} from './log/form-record/form-record.component';
-import {HomepageComponent} from './log/homepage/homepage/homepage.component';
-import {UserProfileComponent} from './user-profile/user-profile.component';
-import {OffersComponent} from './offers/offers.component';
+import { AuthComponent } from './log/auth/auth.component';
+import { FormRecordComponent } from './log/form-record/form-record.component';
+import { HomepageComponent } from './log/homepage/homepage/homepage.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { OffersComponent } from './offers/offers.component';
 import { ExchangeRequestsComponent } from './exchange-requests/exchange-requests.component';
 
 export const routes: Routes = [
   // Definisci le tue rotte qui
-  { path: '', component: UserProfileComponent },
+  { path: '', pathMatch: 'full', component: AuthComponent },
+  { path: 'login', component: AuthComponent },
+  { path: 'register', component: FormRecordComponent },
+  { path: 'home', component: OffersComponent },
+  { path: 'profilo', component: UserProfileComponent },
   { path: 'book-details/:id', component: BookDetailsComponent },
-  {path: '', pathMatch: 'full', component: AuthComponent},
-  {path: 'login', component: AuthComponent},
-  {path: 'register', component: FormRecordComponent},
-  {path: 'home', component: HomepageComponent},
-  {path: 'profilo', component: UserProfileComponent },
-  {path: 'offerte', component: OffersComponent},
-  {path: "scambio", component: ExchangeRequestsComponent}
+  { path: 'scambio', component: ExchangeRequestsComponent },
 ];
 
 @NgModule({
