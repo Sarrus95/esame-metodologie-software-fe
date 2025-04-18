039d970e01168405d378c30ed8e551558ed22d5d
Salvo <scarfi@stevejobs.academy>
2025-04-18 10:23:52 +0200

    l

--- src/app/services/bookservice.service.ts
+++ src/app/services/bookservice.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
+  private API_URL = 'http://localhost:3000/books';
+  private authToken = '';
 
-  constructor(private http: HttpClient) { }
-
-}
+  constructor(private http: HttpClient,private localStorage: LocalStorageService) {
+    this.authToken = this.localStorage.get("authToken") || "";
+  }
 
   getBooks(): Observable<any> {
     const headers = { "Authorization" : this.authToken };
